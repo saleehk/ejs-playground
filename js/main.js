3679c0c22d8a33378cdcc59bc47bf70d57fe8471
saleeh <saleeh93@gmail.com>
2021-08-18 20:13:47 +0530

    added json vars

--- js/main.js
+++ js/main.js
@@ -1,14 +1,54 @@
+const parseTemplate = ({ template, data }) => {
+    const outPutData = { ...template };
+    const attributeKeys = Object.keys(outPutData);
+    attributeKeys.map((item) => {
+        let currentItem = outPutData[item];
+        try {
+            if (typeof currentItem === 'string') {
+                currentItem = currentItem.replace(new RegExp('{{', 'g'), '<%-');
+                currentItem = currentItem.replace(new RegExp('}}', 'g'), '%>');
+                outPutData[item] = ejs.render(currentItem, data);
+                /**
+                 * Bad code; Need a lot of optimization
+                 */
+                if (outPutData[item].match(/object Object/)) {
+                    currentItem = currentItem.replace('<%-', '<%- JSON.stringify(');
+                    currentItem = currentItem.replace('%>', ')%>');
+                    outPutData[item] = ejs.render(currentItem, data);
+                }
+                if (currentItem.match('JSON.stringify')) {
+                    outPutData[item] = JSON.parse(outPutData[item]);
+                }
+            } else if (typeof currentItem === 'object') {
+                const out = parse({ template: currentItem, data });
+                outPutData[item] = out;
+            }
+        } catch (error) {
+            // console.log(error);
+        }
+        return null;
+    });
+    return outPutData;
+};
 (function () {
 
     var $result = document.getElementById("result");
 
-    function update () {
+    function update() {
         var result = null
-          , input = editor.getValue()
-          ;
+            , input = editor.getValue()
+            ;
+        localStorage.setItem('template', input);
 
         try {
-            result = ejs.render(input)
+            const variableValue = editorVars.getValue()
+            const variables = JSON.parse(variableValue)
+            // result = variables
+            const templateResp = parseTemplate({ template: { input }, data: variables })
+            result = `${JSON.stringify(templateResp)}`;
+            console.log(result);
+            // result = result.replace(/\n/g, "\\\\n").replace(/\r/g, "\\\\r").replace(/\t/g, "\\\\t");
+
             $result.parentNode.style.background = "#27ae60";
         } catch (e) {
             result = e.stack;
@@ -21,26 +61,29 @@
             $result.textContent = result;
         }
     }
+    const variables = { blockData: {}, blockItem: {} }
+    const editorVars = ace.edit("editor-vars");
+    editorVars.setValue(
+        localStorage.getItem('data-vars') ||
+        JSON.stringify(variables, null, 2), -1);
 
-    var editor = ace.edit("editor");
+    editorVars.on("change", () => {
+        const input = editorVars.getValue()
+            ;
+        localStorage.setItem('data-vars', input);
+    });
+
+    editorVars.setTheme("ace/theme/monokai");
+    editorVars.getSession().setMode("ace/mode/json");
+
+    const editor = ace.edit("editor");
+
     editor.setTheme("ace/theme/monokai");
     editor.getSession().setMode("ace/mode/ejs");
     editor.on("change", update);
-    editor.setValue(`OK, so have fun! :D
--------------------
-<%
-    var fruits = ["Apple", "Pear", "Orange", "Lemon"]
-      , random = " ".repeat(198).split("").map(x => Math.random())
-      ;
-%>
-
-These fruits are amazing:
-<% for(var i = 0; i < fruits.length; ++i) {%>
-  - <%=fruits[i]%>s<% } %>
-
-Let's see some random numbers:
-
-<% random.forEach((c, i) => {
-%> <%=c.toFixed(10) + ((i + 1) % 6 === 0 ? "\\n": "") %><%});%>`, -1);
+    editor.setValue(
+        localStorage.getItem('template') ||
+        `Hello world`, -1);
     editor.focus();
-})();
+}
+)();
